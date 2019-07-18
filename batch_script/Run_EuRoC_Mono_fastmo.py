1d62f5f8210c35806456843496020572691d72e3
yipuzhao <yzhao347@gatech.edu>
2019-07-18 01:19:42 -0400

    add script for euroc fastmo

--- batch_script/Run_EuRoC_Mono_fastmo.py
+++ batch_script/Run_EuRoC_Mono_fastmo.py
@@ -11,6 +11,8 @@ SeqNameList = ['MH_01_easy', 'MH_02_easy', 'MH_03_medium', 'MH_04_difficult', 'M
 
 Result_root = '/mnt/DATA/tmp/EuRoC/DSO_Mono_Speedx'
 
+Playback_Rate_List = [1.0, 2.0, 3.0, 4.0, 5.0]; # [1.0, 3.0, 5.0] # 
+
 # Optimal param for DSO
 Number_GF_List = [600];
 # Number_GF_List =  [200, 300, 400, 600, 800, 1000, 1500, 2000];
@@ -31,50 +33,53 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-for ri, num_gf in enumerate(Number_GF_List):
+for pi, rate in enumerate(Playback_Rate_List):
+    for ri, num_gf in enumerate(Number_GF_List):
     
-    Experiment_prefix = 'ObsNumber_' + str(int(num_gf))
+        Experiment_prefix = 'ObsNumber_' + str(int(num_gf))
 
-    for iteration in range(0, Num_Repeating):
+        for iteration in range(0, Num_Repeating):
 
-        Experiment_dir = Result_root + Experiment_prefix + '_Round' + str(iteration + 1)
-        cmd_mkdir = 'mkdir -p ' + Experiment_dir
-        subprocess.call(cmd_mkdir, shell=True)
+            Experiment_dir = Result_root + str(rate) + '/' \
+             + Experiment_prefix + '_Round' + str(iteration + 1)
+            cmd_mkdir = 'mkdir -p ' + Experiment_dir
+            subprocess.call(cmd_mkdir, shell=True)
 
-        for sn, sname in enumerate(SeqNameList):
-            
-            print bcolors.ALERT + "====================================================================" + bcolors.ENDC
+            for sn, sname in enumerate(SeqNameList):
+                
+                subprocess.call('clear all', shell=True)
+                print bcolors.ALERT + "====================================================================" + bcolors.ENDC
 
-            SeqName = SeqNameList[sn]
-            print bcolors.ALERT + "Round: " + str(iteration + 1) + "; Seq: " + SeqName
+                SeqName = SeqNameList[sn]
+                print bcolors.ALERT + "Round: " + str(iteration + 1) + "; Seq: " + SeqName
 
-            File_Calib = Path_DSO_Calib + '/EuRoC_Mono_calib.txt'
-            File_Gamma = ' '
-            File_Vignette = ' '
-            Misc_Config = ' mode=1 nolog=1 quiet=1 nogui=1'
+                File_Calib = Path_DSO_Calib + '/EuRoC_Mono_calib.txt'
+                File_Gamma = ' '
+                File_Vignette = ' '
+                Misc_Config = ' mode=1 nolog=1 quiet=1 nogui=1'
 
-            File_rosbag  = '/mnt/DATA/Datasets/EuRoC_dataset/BagFiles/' + SeqName + '.bag'
-            File_traj = Experiment_dir + '/' + SeqName
+                File_rosbag  = '/mnt/DATA/Datasets/EuRoC_dataset/BagFiles/' + SeqName + '.bag'
+                File_traj = Experiment_dir + '/' + SeqName
 
-            cmd_slam   = str('rosrun dso_ros dso_live image:=' + '/cam0/image_raw' + ' calib=' + File_Calib + ' gamma=' + File_Gamma + \
-                ' vignette=' + File_Vignette + ' preset='  + str(int(num_gf)) + ' realtime=' + File_traj + Misc_Config)
-            cmd_rosbag = 'rosbag play ' + File_rosbag # + ' -r 0.3' # + ' -u 20' 
+                cmd_slam   = str('rosrun dso_ros dso_live image:=' + '/cam0/image_raw' + ' calib=' + File_Calib + ' gamma=' + File_Gamma + \
+                    ' vignette=' + File_Vignette + ' preset='  + str(int(num_gf)) + ' realtime=' + File_traj + Misc_Config)
+                cmd_rosbag = 'rosbag play ' + File_rosbag + ' -r ' + str(rate) # + ' -u 20' 
 
-            print bcolors.WARNING + "cmd_slam: \n"   + cmd_slam   + bcolors.ENDC
-            print bcolors.WARNING + "cmd_rosbag: \n" + cmd_rosbag + bcolors.ENDC
+                print bcolors.WARNING + "cmd_slam: \n"   + cmd_slam   + bcolors.ENDC
+                print bcolors.WARNING + "cmd_rosbag: \n" + cmd_rosbag + bcolors.ENDC
 
-            print bcolors.OKGREEN + "Launching SLAM" + bcolors.ENDC
-            proc_slam = subprocess.Popen(cmd_slam, shell=True)
-            # proc_slam = subprocess.call(cmd_slam, shell=True)
-            time.sleep(SleepTime)
+                print bcolors.OKGREEN + "Launching SLAM" + bcolors.ENDC
+                proc_slam = subprocess.Popen(cmd_slam, shell=True)
+                # proc_slam = subprocess.call(cmd_slam, shell=True)
+                time.sleep(SleepTime)
 
-            print bcolors.OKGREEN + "Launching rosbag" + bcolors.ENDC
-            proc_bag = subprocess.call(cmd_rosbag, shell=True)
+                print bcolors.OKGREEN + "Launching rosbag" + bcolors.ENDC
+                proc_bag = subprocess.call(cmd_rosbag, shell=True)
 
-            print bcolors.OKGREEN + "Wait for exporting results" + bcolors.ENDC
-            time.sleep(SleepTime)
+                print bcolors.OKGREEN + "Wait for exporting results" + bcolors.ENDC
+                time.sleep(SleepTime)
 
-            print bcolors.OKGREEN + "Finished rosbag playback, kill the process" + bcolors.ENDC
-            subprocess.call('rosnode kill dso_live', shell=True)
-            time.sleep(SleepTime)
-            subprocess.call('pkill dso_live', shell=True)
+                print bcolors.OKGREEN + "Finished rosbag playback, kill the process" + bcolors.ENDC
+                subprocess.call('rosnode kill dso_live', shell=True)
+                time.sleep(SleepTime)
+                subprocess.call('pkill dso_live', shell=True)
